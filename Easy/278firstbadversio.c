dc46121a032be1759c20bd433685e8d12be4f7f4
Manasvi Srivastava <114558299+grimreapermanasvi@users.noreply.github.com>
2024-02-29 22:39:57 +0530

    first bad version

---
+++ Easy/278firstbadversio.c
@@ -0,0 +1,16 @@
+// The API isBadVersion is defined for you.
+ bool isBadVersion(int version);
+
+int firstBadVersion(int n) {
+    int left, right, mid;
+    left=1; right =n;
+    while(left<right){
+        mid= left+(right-left)/2;
+        if(isBadVersion(mid)){
+            right =  mid ;
+        }else{
+            left = mid + 1;
+        }
+    }
+    return right ;
+}
