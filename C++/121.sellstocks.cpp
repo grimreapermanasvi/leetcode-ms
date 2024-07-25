73a566f6d58e56efca3e7aaa7bbc237131269d92
Manasvi Srivastava <114558299+grimreapermanasvi@users.noreply.github.com>
2024-07-25 19:38:28 +0530

    new

---
+++ C++/121.sellstocks.cpp
@@ -0,0 +1,15 @@
+//Optimal Solution TC O(n)//
+/*class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int mini=INT_MAX;
+        int maxprofit=0;
+        for(int i=0;i<prices.size();i++)
+        {
+            int cost=prices[i]-mini;
+            maxprofit=max(maxprofit,cost);
+            mini=min(mini,prices[i]);
+        }
+        return maxprofit;
+    }
+};*/
