7d6c142a7cb482119e72bfc9dd0cb50cdcc978ff
Manasvi Srivastava <114558299+grimreapermanasvi@users.noreply.github.com>
2025-01-21 01:28:53 +0530

    all gfg questions done till now

---
+++ C++/Maxscorewithminsubarray(gfg).cpp
@@ -0,0 +1,11 @@
+/*int pairWithMaxSum(vector<int> &arr) {
+        if (arr.size() < 2) return -1;  
+        int maxi = INT_MIN;
+        
+        for (int i = 0; i < arr.size() - 1; i++) {
+            int sum = arr[i] + arr[i + 1];  
+            maxi = max(maxi, sum);  
+        }
+        
+        return maxi;
+    }*/
