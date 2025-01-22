e9a51cbe91e1665ddb481dd8eadcfa5b8431f113
Manasvi Srivastava <114558299+grimreapermanasvi@users.noreply.github.com>
2025-01-23 02:02:31 +0530

    mixed

---
+++ C++/floorinsortedarr(gfg).cpp
@@ -0,0 +1,17 @@
+/*int findFloor(vector<int>& arr, int k) {
+        int n = arr.size();
+        int low = 0, high = n - 1;
+        int ans = -1;
+    
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+    
+            if (arr[mid] <= k) {
+                ans = mid;  
+                low = mid + 1; 
+            } else {
+                high = mid - 1; 
+            }
+        }
+        return ans;
+    }*/
